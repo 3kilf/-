b083b8be3156229de73bda676569f4d28df753bf
3kilf <olga.piskunova.18.06@mail.ru>
2026-06-11 08:29:33 +0300

    Create script.js

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+
+setTimeout(function() {
+    document.querySelectorAll('.alert').forEach(function(el) {
+        el.style.transition = 'opacity 0.5s';
+        el.style.opacity = '0';
+        setTimeout(function() { el.remove(); }, 500);
+    });
+}, 4000);
